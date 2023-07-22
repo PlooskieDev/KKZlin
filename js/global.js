cf943cb5c3bb29296002bf10254eb713193bdce6
Michael Pluskal <m_pluskal@utb.cz>
2023-07-23 00:16:15 +0200

    Added swiping to open the side menu

--- js/global.js
+++ js/global.js
@@ -1,6 +1,26 @@
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
+let touchstartX = 0
+let touchendX = 0
+
+function swipe() {
+    if (touchendX > touchstartX) {
+        if (touchendX > -1200 && touchstartX < -1350) {
+            document.getElementById('swipeButton').dispatchEvent(new Event('click'));
+        }
+    }
+}
+
+document.addEventListener('touchstart', e => {
+    touchstartX = e.changedTouches[0].screenX
+})
+
+document.addEventListener('touchend', e => {
+    touchendX = e.changedTouches[0].screenX
+    swipe()
+})
+
 myModal.addEventListener('shown.bs.modal', () => {
     myInput.focus()
 })
